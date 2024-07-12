807d241de58abf32a3964325ac2f9ae937ad589c
quabwww <josuebuenovargas@gmail.com>
2024-07-11 19:00:12 -0500

    d

--- etc.py
+++ etc.py
@@ -3,11 +3,11 @@ import requests
 n = 2
 if n == 1:
     paran = {}
-    json = {"token": "TOKEN BOT"}
+    json = {"token": ""}
     url = requests.post("http://localhost:8000/start-bot", json=json)
 
 if n == 2:
-    json = {"session_id": "9b198ae8-3236-40d9-9104-d7fd74c3a68b", "user_id": 1073383604576591974, "guild_id": 1077968892535775262, "channel_id": 1100148368996573265, "music": "https://www.youtube.com/watch?v=nyXffW_RQI4", "folder": "10000"}
+    json = {"session_id": "bd73f491-4402-403d-9aaf-8c64f4540110", "user_id": 487430318500872203, "guild_id": 1077968892535775262, "channel_id": 1100148368996573265, "music": "https://www.youtube.com/watch?v=nyXffW_RQI4", "folder": "10000"}
     url = requests.post("http://localhost:8000/play-music", json=json)
 
 
